cf7bcd65e306c25794460e2a1660db3de4ccba16
Valeriafilipeva <lerikafilipeva@gmail.com>
2024-02-13 14:52:29 +0300

    lab2

--- index.php
+++ index.php
@@ -1,42 +1,37 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lab №1</title>
-    <style>
-        .header {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            padding: 23px;
-        }
-        .text1 {
-            width: 100%;
-            text-align: center;
-        }
-    </style>
+    <title>Lab2</title>
 </head>
 <body>
-    <header class="header">
-        <div class="logo">
-            <img src="./img/Logo_Polytech_rus_main.jpg" width="150px" alt="Логотип">
-        </div>
-        <div class="text1">
-            <h1>Домашняя работа: Hello, World!</h1>
-        </div>
-    </header>
-    <main>
-      <?php
-        echo "<h1>Hello world</h1>";
-        $a = 27;
-        $b = 12;
-        $hypotenuse = round(hypot($a, $b), 2); 
-        echo $hypotenuse;
-      ?>
-    </main>
-    <footer>
-        <h2>Задание для самостоятельной работы</h2>
-    </footer>
+    <?php 
+        $equation = 'X * 9 = 56';
+        $elem = explode (" ", $equation);
+        $left = $elem[0];
+        $right = $elem[2];
+        $result = $elem[4];
+        switch ($elem[1]) {
+            case "+":
+                $x = $result - $right;
+                break;
+            case "-":
+                $x = $result + $right;
+                break;
+            case "*":
+                $x = $result / $right;
+                break;
+            case "/":
+                $x = $result * $right;
+                break;
+            case "**":
+                $x = log($res, $right);
+                break;
+            default:
+                echo "Неподдерживаемй оператор";
+        }
+        echo $x;
+    ?>
 </body>
 </html>
